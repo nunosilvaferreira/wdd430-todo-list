b208ace0b7a217ae0b005856391b6e828bec9c5e
nunosilvaferreira <nferreira@byupathway.edu>
2025-11-12 21:00:01 +0000

    App language 2.2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wdd430-todo-list/'
+  base: '/wdd430-todo-list/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
